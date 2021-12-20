c97ba58de295b95886d5d19bc04a4965de2e1c2a
poiuy721 <dkavnd@naver.com>
2021-12-20 17:09:28 +0900

    add more queries

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,11 @@
-import {getMovies,} from "./db.js"
+import { getMovies, getMovie, getSuggestions } from "./db";
 
-const resolvers={
-    Query:{
-        movies:(_,{limit,rating})=>getMovies(limit,rating),
-    }
+const resolvers = {
+  Query: {
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
+  }
 };
 
 export default resolvers;
